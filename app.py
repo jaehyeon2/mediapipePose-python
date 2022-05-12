f83f7efbc7ffa3f0025a1106546f9b61a6a7f45b
jaehyeon2 <wol5971@gmail.com>
2022-05-12 13:14:37 +0900

    slinky

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+from urllib import request
+
 from flask import Flask, render_template
+
+import logging
+logger = logging.getLogger("app")
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('main.html')
+
+@app.route('/link')
+def link():
+    return render_template('link.html')
+
+@app.route('/slinky')
+def slinky():
+    link = request.form.get("link")
+    print(link)
+
+    return render_template('pose.html')
 
 if __name__=='__main__':
     app.run(debug=True)
